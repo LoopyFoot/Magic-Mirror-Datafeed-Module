9fe2bf3121a17331e097ee38088ebcfb90aa4a4a
Sergey Morozov <sergey@slack-corp.com>
2017-01-25 02:46:12 -0800

    reworked datafeed module code

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module: 'datafeed',
-	position: 'middle',
-	config{
-		remoteFile: 'testfile.csv'
-		}
+position: 'middle_center',
+config: {
+	remoteFile: 'datatest2.csv', // single column text file with data, must be in the datafeed module directory
+	updateInterval: 60 * 1000,  // updates every minute
+	initialLoadDelay: 0, // 0 milliseconds delay
+	animationSpeed: 2000 // speed to animate update transition in ms
+}
